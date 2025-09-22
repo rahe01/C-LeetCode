0d1f108d8c8b8c6daa0b57c08ed9f6f1e327e487
rahe01 <rahatrahe01@gmail.com>
2025-09-22 11:57:30 +0600

    f

---
+++ Pattern/reversetriangle.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+   int loop =1;
+    for(int i=0; i<n; i++){
+     
+        for(int j = i+1 ; j>0; j--){
+            cout << j<<" ";
+            
+          
+        }
+        
+        cout<< endl;
+    }
+    
+    return 0;
+}
